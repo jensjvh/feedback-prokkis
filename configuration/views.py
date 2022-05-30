002acc739058f0a7040359f4b47773f81d2f5db7
bee61 <jens.johann.helin@gmail.com>
2022-05-31 00:44:06 +0300

    Put method

--- configuration/views.py
+++ configuration/views.py
@@ -26,7 +26,7 @@ class productRequestsList(APIView):
 
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, format = None):
+    def delete(self, request):
 
         productrequest_delete = productRequests.objects.filter(title = request.data.get('title'))
         if not productrequest_delete:
@@ -39,3 +39,24 @@ class productRequestsList(APIView):
             {"res": "Object deleted!"},
             status = status.HTTP_200_OK
         )
+    
+    def put(self, request):
+
+        productrequest_update = productRequests.objects.filter(title = request.data.get('title'))
+        if not productrequest_update:
+            return Response(
+                {"res": "Object with title does not exist"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'title' : request.data.get('title'),
+            'category' : request.data.get('category'),
+            'upvotes' : request.data.get('upvotes'),
+            'status' : request.data.get('status'),
+            'description' : request.data.get('description')
+        }
+        serializer = productRequestsSerializer(instance = productrequest_update, data = data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
