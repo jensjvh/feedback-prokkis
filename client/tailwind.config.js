f2bda963da41ebc5b16993d392ab3e305d218192
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-06-03 03:34:30 +0300

    Changed the plan. I'm going to start developing mobile first and then expanding into computer use. I also added the frontendmentor assets into the project. I created the typography and colorgraphy from the given Figma design system.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  important: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkpurple: "#AD1FEA",
+        blue: "#4661E6",
+        darkblue: "#373F68",
+        bluewhite: "#F2F4FF",
+        almostwhite: "#F7F8FD",
+        darkbluelighter: "#3A4374",
+        grayblue: "#647196",
+        orange: "#F49F85",
+        lightblue: "#62BCFA",
+      },
+    },
   },
   plugins: [],
 };
